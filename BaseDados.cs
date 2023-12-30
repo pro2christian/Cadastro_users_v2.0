52383147e73b57fface594f0a284240d9422573c
Christian Oliveira da Silva <pro2christian@icloud.com>
2023-12-30 17:56:58 -0300

    base-dados_v1

---
+++ BaseDados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_users_v2._0
+{
+    internal class BaseDados
+    {
+       private List<CadastroCliente> cadastroCliente;
+
+        public void AddCliente(CadastroCliente pCliente)
+        {
+            cadastroCliente.Add(pCliente);
+        }
+        public List<CadastroCliente> BuscaCliente_Doc( string pNumeroDocumento)
+        {
+            List<CadastroCliente> tempCadastroClientes = cadastroCliente.Where(cliente => cliente.NumeroDocumento == pNumeroDocumento).ToList();
+            if (tempCadastroClientes.Count > 0)
+                return tempCadastroClientes;
+            else
+                return null; 
+        }
+        public List<CadastroCliente> ExcluiCliente_Doc( string pNumeroDocumento)
+        {
+            List<CadastroCliente> tempCadastroClientes = cadastroCliente.Where(cliente => cliente.NumeroDocumento == pNumeroDocumento).ToList();
+            if (tempCadastroClientes.Count > 0)
+            {
+                foreach(CadastroCliente clienteRemove in tempCadastroClientes)
+                {
+                    tempCadastroClientes.Remove(clienteRemove);
+                }
+                return tempCadastroClientes;
+            }
+            else 
+                return null; 
+        }
+        public BaseDados()
+        {
+            cadastroCliente = new List<CadastroCliente>();
+        }
+    }
+}
